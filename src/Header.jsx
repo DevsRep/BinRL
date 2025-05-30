60814ca50de805645f8565d45527c5126adeb041
Devesh Keshavan S <deveshkeshavan@gmail.com>
2025-05-30 19:48:26 +0530

    UI updates to look previous links

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,39 @@
 function Header(){
 
+
+    const handlePrevLinksBtn = () =>{
+        const cont = document.querySelector(".prev-links-list-cont")
+        if(cont.style.width == "0px"){
+            cont.style.width = "500px"
+        }else{
+            cont.style.width = "0px"
+        }
+
+    }
+
     return(
         <header>
-            <div className="appName">BinRL</div>
+            <h1 className="appName">BinRL</h1>
 
-            {/* <div className="navLinks">
+            <div className="navLinks">
+                <div onClick={handlePrevLinksBtn}>Your Links</div>
+                <div>LinkDir</div>
+            </div>
 
-            </div> */}
+            <div className="prev-links-list-cont">
+                <div className="close-btn" onClick={handlePrevLinksBtn}>&times;</div>
+                <div className="prev-link">
+                    Actual Link:
+                    <div className="actual-link">www.google.com</div>
+                    Shortened Link:
+                    <div className="shortened-link">comprl.web.app/short</div>
+                </div>
+                <div className="prev-link">
+                    Link1
+                </div>
+            </div>
+
+
         </header>
     )
 
